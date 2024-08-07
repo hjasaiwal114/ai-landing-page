4c03cf5989dd28f40fc838360ff21f85253c3e0b
Himanshu <hjasaiwal114@gmail.com>
2024-08-07 23:07:37 +0530

    adding starts as background

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,5 +1,6 @@
 import LogoIcon from "@/assets/logo.svg";
 import MenuIcon from "@/assets/icon-menu.svg";
+import {Button} from "@/components/Button";
 
 export const Header = () => {
   return ( 
@@ -20,14 +21,7 @@ export const Header = () => {
           </nav>
         </div>
         <div className="flex gap-4 items-center ">
-          <button className="realtive py-2 px-3  font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
-            <div className="absolute inset-0">
-              <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-graient(to_bottom,black,transparent)]"></div>
-              <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)] "></div>
-              <div className="absoulte inset-0 shadow-[0_0_10px_rgb(140,69,255,.7)_inset] rounded-lg"></div>
-            </div>
-            <span>join waitlist</span>
-            </button>
+          <Button>Join waitlist</Button>
           <MenuIcon className="md:hidden"/>
         </div>
       </div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,26 @@
+import { Button } from "@/components/Button";
+import starsBig from '@/assets/stars.png';
+
 export const Hero = () => {
-  return <section>Hero</section>;
+  return (
+  <section className="h-[492px] flex items-center" 
+   style={{
+      backgroundImage: `url(${starsBig.src})`,
+   }}
+  >
+    <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/30 top-1/2 left-1/2"></div>
+    <div className="container">
+      <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
+        AI SEO
+      </h1>
+      <p className="text-lg text-white/70 mt-5 text-center">
+        Elevate your site's visibility effortlessly with AI,
+        where smart technology meets user-friendly SEO tools.
+      </p>
+      <div className="flex justify-center mt-5">
+        <Button>Join waitlist</Button>
+      </div>
+    </div>
+  </section>
+  )
 };
