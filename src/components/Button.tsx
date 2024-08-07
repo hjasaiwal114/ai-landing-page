0197dc52c3e5892df0b4e2bc4e5d8185bc6b8de0
Himanshu <hjasaiwal114@gmail.com>
2024-08-07 23:25:51 +0530

    adding gradient background

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 export const Button = (props: React.PropsWithChildren) => {
     return (
-        <button className="realtive py-2 px-3  font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
+        <button className="relative py-2 px-3  font-medium text-sm bg-gradient-to-b from-[#190d2e] to-[#4a208a] shadow-[0px_0px_12px_#8c45ff]">
             <div className="absolute inset-0">
               <div className="rounded-lg border border-white/20 absolute inset-0 [mask-image:linear-graient(to_bottom,black,transparent)]"></div>
               <div className="rounded-lg border absolute inset-0 border-white/40 [mask-image:linear-gradient(to_top,black,transparent)] "></div>
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -8,8 +8,8 @@ export const Hero = () => {
       backgroundImage: `url(${starsBig.src})`,
    }}
   >
-    <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/30 top-1/2 left-1/2"></div>
-    <div className="container">
+    <div className="absolute h-64 w-64 bg-purple-500 rounded-full border border-white/30 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-[radial-gradient(50%_50%_at_16.8%_18.3%,white,rgb(184,148,255)_37.7%,rgb(24,0,66))]"></div>
+    <div className="container relative">
       <h1 className="text-8xl font-semibold tracking-tighter bg-white bg-[radial-gradient(100%_100%_at_top_left,white,white,rgb(74,32,138,.5))] text-transparent bg-clip-text text-center">
         AI SEO
       </h1>
