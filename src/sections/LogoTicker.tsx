6348eabd2d6910a1c6ce95fbf6d652010e8204f6
Himanshu <hjasaiwal114@gmail.com>
2024-08-14 08:28:41 +0530

    anaimated logo ticker

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,27 +1,51 @@
-import acmeLogo from '@/assets/logo-acme.png';
-import apexlogo from '@/assets/logo-apex.png';
-import celestianLogo from '@/assets/logo-celestial.png';
-import quantamLogo from '@/assets/logo-quantum.png';
-import pulseLogo from '@/assets/logo-pulse.png';
-import echoLogo from '@/assets/logo-echo.png';
+"use client";
+import acmeLogo from "@/assets/logo-acme.png";
+import apexlogo from "@/assets/logo-apex.png";
+import celestianLogo from "@/assets/logo-celestial.png";
+import quantamLogo from "@/assets/logo-quantum.png";
+import pulseLogo from "@/assets/logo-pulse.png";
+import echoLogo from "@/assets/logo-echo.png";
+import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
     <section className="py-20 md:py-24">
-    <div className="container ">
-      <div className='flex item-center'>
-        <div className='flex-1 md:flex-none gap-5'>
-          <h2>Trusted by top inovative teams</h2>
-        </div>
-        <div className="flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
-        <div className="flex flex-none gap-14 ">
-          {[acmeLogo, pulseLogo, echoLogo, celestianLogo, apexlogo, quantamLogo,].map((logo) => (
-            <img src={logo.src} key={logo.src} className= "h-6 w-auto" />
-          ))}
-        </div>
+      <div className="container ">
+        <div className="flex item-center">
+          <div className="flex-1 md:flex-none gap-5">
+            <h2>Trusted by top inovative teams</h2>
+          </div>
+          <div className=" flex flex-1 overflow-hidden [mask-image:linear-gradient(to_right,transparent,black_20%,black_80%,transparent)]">
+            <motion.div
+              initial={{ translateX: "-50%" }}
+              animate={{ translateX: "0" }}
+              transition={{
+                repeat: Infinity,
+                duration: 30,
+                ease: "linear",
+              }}
+              className="flex flex-none gap-14 pr-14 -translate-x-1/2"
+            >
+              {[
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestianLogo,
+                apexlogo,
+                quantamLogo,
+                acmeLogo,
+                pulseLogo,
+                echoLogo,
+                celestianLogo,
+                apexlogo,
+                quantamLogo,
+              ].map((logo) => (
+                <img src={logo.src} key={logo.src} className="h-6 w-auto" />
+              ))}
+            </motion.div>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
   );
 };
